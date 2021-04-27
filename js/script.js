afd7628ffe5ed0aba419f9054ae9b735e57844ea
Kacper Przybyszewski <kacper.przybyszewskiiii@gmail.com>
2021-04-27 11:24:13 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+{
+    const tasks = [{
+            content: "przerobić lekcje z kursu",
+            done: false,
+        },
+        {
+            content: "zjeść makaron z pesto",
+            done: true,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+
+}
